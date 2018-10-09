4b4a3b795a05215f5be1694ce1ac50618bca2e7a
Frank Meza <frankmezapdx@gmail.com>
2018-10-09 16:54:39 -0700

    moved file into redux/

--- packages/common/src/redux/index.ts
+++ packages/common/src/redux/index.ts
@@ -1,9 +1,12 @@
 import * as redux from "redux"
 import { applyMiddleware, createStore, Middleware } from "redux"
 import { createLogger } from "redux-logger"
-import { actions, counter } from "./redux/modules/counter"
+// import { actions, counter } from "./redux/modules/counter"
+import { actions, counter } from "../redux/modules/counter"
 
+// begin imports without special characters
 import createSagaMiddleware from "redux-saga"
+import rootSaga from "../redux/sagas"
 
 export interface IAppState {
     readonly counter: number
@@ -15,18 +18,18 @@ const appState: IAppState = {
 
 const INITIAL_STATE: IAppState = appState
 
-const appReducer = redux.combineReducers({ counter })
+const appReducer: redux.Reducer = redux.combineReducers({ counter })
 const logger = createLogger({ stateTransformer: (state: IAppState) => state })
 const sagaMiddleware = createSagaMiddleware()
 const middleware: ReadonlyArray<Middleware> = [sagaMiddleware, logger]
 
 const createStoreWithMiddleware = createStore(
     appReducer,
-    INITIAL_STATE,
+    INITIAL_STATE as redux.DeepPartial<{}>,
     applyMiddleware(...middleware),
 )
 
 const store = createStoreWithMiddleware
-// sagaMiddleware.run(Sagas.root)
+sagaMiddleware.run(rootSaga)
 
 export { actions, appState, store, INITIAL_STATE }
