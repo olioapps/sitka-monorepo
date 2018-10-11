347cf2c4c899ef736b4ca376d5058dda7949ea22
Frank Meza <frankmezapdx@gmail.com>
2018-10-11 16:24:13 -0700

    adjust and refine store-related stuff

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,4 +1,4 @@
-import { store } from "@cashew/common"
+import { actions, runMiddleware, storeCreatorWrapper } from "@cashew/common"
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
@@ -6,9 +6,13 @@ import App from "./App"
 import "./index.css"
 import registerServiceWorker from "./registerServiceWorker"
 
+export const store = storeCreatorWrapper()()
+
+runMiddleware()
+
 ReactDOM.render(
     <Provider store={store}>
-        <App appState={store.getState()} />
+        <App actions={actions} appState={store.getState()} />
     </Provider>,
     document.getElementById("root") as HTMLElement,
 )
