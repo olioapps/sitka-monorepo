92c57e1516be71cec790d5b72f852be25d4be1d6
Frank Meza <frankmezapdx@gmail.com>
2018-10-18 15:02:06 -0700

    add type for store

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -10,10 +10,13 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import App from "./App"
+import { Store, AnyAction } from "redux"
 import "./index.css"
 import registerServiceWorker from "./registerServiceWorker"
 
-export const store = storeCreatorWrapper()()
+type ReduxStore = Store<{}, AnyAction> & { dispatch: {} }
+
+export const store: ReduxStore = storeCreatorWrapper(/* clientMiddleware */)()
 
 runMiddleware()
 
